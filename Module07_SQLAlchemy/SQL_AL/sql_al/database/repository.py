289d3dc9d270109f0cb5c8cff9a1fe425339b915
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-04-09 19:55:30 +0200

    update repository

--- Module07_SQLAlchemy/SQL_AL/sql_al/database/repository.py
+++ Module07_SQLAlchemy/SQL_AL/sql_al/database/repository.py
@@ -1,18 +1,42 @@
-def get_user():
-    pass
+from sqlalchemy import and_
+
+from database.db import session
+from database.models import User, Todo
 
 
-def get_all_todos():
-    pass
+def get_user(login):
+    user = session.query(User).filter(User.login == login).first()
+    return user
 
 
-def create_todo():
-    pass
+def create_todo(title, description, user):
+    todo = Todo(title=title, description=description, user=user)
+    session.add(todo)
+    session.commit()
+    session.close()
 
 
-def update_todo():
-    pass
+def get_all_todos(user):
+    todos = (
+        session.query(Todo).join(User).filter(Todo.user == user).all()
+    )  # Filter == where session.query(Todo).join(User).where(Todo.user == user).all()
+    return todos
 
 
-def remove_todo():
-    pass
+def update_todo(_id, title, description, user):
+    todo = session.query(Todo).filter(and_(Todo.user == user, Todo.id == _id))
+    if todo:
+        todo.update({"title": title, "description": description})
+        session.commit()
+    session.close()
+    return todo.first()
+
+
+def remove_todo(_id, user):
+    r = (
+        session.query(Todo)
+        .join(User)
+        .filter(and_(Todo.user == user, Todo.id == _id))
+        .delete()
+    )
+    return r
