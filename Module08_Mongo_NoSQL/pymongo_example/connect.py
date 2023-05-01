2aff25bc9679405efed652487a06914367b9e448
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-05-01 13:49:09 +0200

    Updating

--- Module08_Mongo_NoSQL/pymongo_example/connect.py
+++ Module08_Mongo_NoSQL/pymongo_example/connect.py
@@ -1,7 +1,19 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import configparser
 
-uri = "mongodb+srv://Thoughtseize_:JYX9gGFssSqILal9@nickitm.rj1lqrb.mongodb.net/?retryWrites=true&w=majority"
+
+config = configparser.ConfigParser()
+config.read("mongoengine\config.ini")
+
+mongo_user = config.get("DB", "user")
+mongodb_pass = config.get("DB", "pass")
+db_name = config.get("DB", "db_name")
+domain = config.get("DB", "domain")
+
+# connect to cluster on AtlasDB with connection string
+
+uri = f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority"""
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"))
