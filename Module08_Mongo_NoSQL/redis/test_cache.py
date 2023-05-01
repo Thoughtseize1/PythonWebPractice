25ed4acc0de08a1a4b14aead24708f8e14a6f903
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-05-01 15:44:11 +0200

    Testing how cache works

---
+++ Module08_Mongo_NoSQL/redis/test_cache.py
@@ -0,0 +1,28 @@
+import redis
+from redis_lru import RedisLRU
+
+r = redis.Redis(host="localhost", port=6379, password=None)
+
+r.set("foo", "bar")
+value = r.get("foo")
+print(value)  # bar
+
+
+client = redis.StrictRedis(host="localhost", port=6379, password=None)
+
+#! 1 minute for cache using default_ttl parametr
+cache = RedisLRU(client, default_ttl=1, max_size=3)
+
+
+@cache
+def f(x):
+    print(f"Function call f({x})")
+    return x
+
+
+if __name__ == "__main__":
+    print(f"Result f(3): {f(3)}")
+    print(f"Result f(3): {f(3)}")
+    print(f"Result f(4): {f(4)}")
+    print(f"Result f(4): {f(4)}")
+    cache.clear_all_cache()
